94bda6b82998c7ddd1a0364446de70d01a876319
DavidLoera <86201527+DavidLoera@users.noreply.github.com>
2022-05-30 16:38:18 -0500

    V 0.2

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,23 @@ window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("abajo", window.scrollY>0);
 })
+
+
+
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () =>{
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", ()=>{
+    hamburguer.classList.remove("active");
+    navMenu.classList.remove("active")
+}))
+
+window.addEventListener("scroll", function(){
+    const menu = document.querySelector(".nav-menu");
+    menu.classList.toggle("abajo", window.scrollY>0);
+})
